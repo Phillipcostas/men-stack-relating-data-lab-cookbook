7ba8e7b1dde285a6beb9dd927359c0ab5863c1cb
PhillipCostas <Phillipcostas@gmail.com>
2024-06-08 16:05:15 -0400

    data not being sent to mongoDB

--- controllers/foods.js
+++ controllers/foods.js
@@ -2,10 +2,38 @@ const express = require('express');
 const router = express.Router();
 
 const User = require('../models/user.js')
+ 
+
+router.get('/', async (req, res) => {
+    try {
+      const currentUser = await User.findById(req.session.user._id);
+      res.render('foods/index.ejs', {
+        pantry: currentUser.pantry,
+      });
+    } catch (error) {
+
+      console.log(error)
+      res.redirect('/')
+    }
+  });
 
 
-router.get('/', (req, res) => {
-    res.render('foods/index.ejs')
+router.get('/new', async (req, res) => {
+    res.render('foods/new.ejs')
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._Id); 
+        currentUser.pantry.push(req.body);
+        await currentUser.save()
+        res.redirect(`/users/${currentUser._Id}/foods`);
+    } catch (error) {
+        console.log(error);
+        res.redirect('/');
+    }
 })
+
+
 
 module.exports = router; 

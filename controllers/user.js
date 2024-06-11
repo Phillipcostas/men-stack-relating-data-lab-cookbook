928f5e656fe1048ea5053217989a1ac1bac75085
PhillipCostas <Phillipcostas@gmail.com>
2024-06-11 11:10:06 -0400

    working on the exploring our community page

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const User = require('../models/user.js')
+const User = require('../models/user.js');
 
 
 router.get('/index', async (req, res) => {
+    const users = await User.find()
     res.render('users/index.ejs')
 });
 
